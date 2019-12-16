bc0837834230ba064d009e5190c22e8f12457826
art1shok <kvitkaart554@gmail.com>
2019-12-16 13:23:56 +0200

    Updates

---
+++ precache-manifest.c3fb94f365154f19634fa5870d11314c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b495d949a95a407243de00072aa5065",
+    "url": "/react_todo-app/index.html"
+  },
+  {
+    "revision": "1a2e3d69911391e2f49e",
+    "url": "/react_todo-app/static/css/main.79067980.chunk.css"
+  },
+  {
+    "revision": "5a76bad6fc1e05f761a2",
+    "url": "/react_todo-app/static/js/2.583b85e5.chunk.js"
+  },
+  {
+    "revision": "1a2e3d69911391e2f49e",
+    "url": "/react_todo-app/static/js/main.d5bef495.chunk.js"
+  },
+  {
+    "revision": "51af3b3c9c5f8ccb1c53",
+    "url": "/react_todo-app/static/js/runtime~main.df364381.js"
+  }
+]);
